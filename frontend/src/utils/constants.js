03e6a58b93c1624301988dac654918a03b5565c3
Alena Ismagilova <ismagilovaalena90@yandex.ru>
2023-05-13 13:37:19 +0400

    fix

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,4 @@
-// export const URL = "http://api.kpd.alena.nomoredomains.monster";
-export const URL = "http://localhost:4000";
+export const URL = "http://api.kpd.alena.nomoredomains.monster";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
